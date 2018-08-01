abc11f80b1bedbdc78b899cb6321d57d0600fbe8
bchamarthi <brchamarthi29@gmail.com>
2018-08-01 20:08:47 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,11 @@ class ApplicationController < Sinatra::Base
   	set :public_dir, "public"
   end
 
-  get "/" do
-  	erb :index
+  get '/' do
+  	erb :homebase
+  end
+  
+  get '/peaceofmind'do
+    erb :peaceofmind
   end
 end
