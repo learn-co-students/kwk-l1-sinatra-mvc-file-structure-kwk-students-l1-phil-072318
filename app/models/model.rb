c924d640e61a4964ceabaf4d3ef956a4a565c2f6
Bridget Kienzle <bridgetkien@gmail.com>
2018-08-01 20:24:40 +0000

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,36 @@
-class Model
-  # Replace with Dog clas
-end
+@@iron_man = 0
+@@iron_man2 = 0
+@@iron_man3 = 0
+@@thor = 0
+@@thorTDW = 0
+@@thor = 0
+@@thorRag = 0
+@@hulk = 0
+@@captain_americaFA = 0
+@@captain_americaTWS = 0
+@@captain_americaCW = 0
+@@antman = 0
+@@antmanATW = 0
+@@avengers = 0
+@@avengersAOU = 0
+@@avengersIW = 0
+@@gotg = 0
+@@gotg2 = 0
+@@doctor_strange = 0
+@@captain_marvel = 0
+@@black_panther = 0
+@@SMHomecoming  = 0
+
+@@TDK = 0
+@@TDKR = 0
+@@MOS = 0
+@@green_lantern = 0
+@@ww = 0
+@@ww1984 = 0
+@@shazam = 0
+@@BMvsSM = 0
+@@justice = 0
+@@suicide_squad = 0
+
+def movies()
+  if 
