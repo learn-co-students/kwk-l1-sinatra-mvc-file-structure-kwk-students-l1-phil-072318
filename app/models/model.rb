f3525e6e266c5fc5ac87a375a36f987948ed1186
Bridget Kienzle <bridgetkien@gmail.com>
2018-08-01 20:44:45 +0000

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -32,5 +32,58 @@
 @@justice = 0
 @@suicide_squad = 0
 
-def movies()
-  if 
+def marvordc(string)
+  if string == "Marvel"
+@@iron_man += 1
+@@iron_man2 += 1
+@@iron_man3 += 1
+@@thor += 1
+@@thorTDW += 1
+@@thorRag += 1
+@@hulk += 1
+@@captain_americaFA += 1
+@@captain_americaTWS += 1
+@@captain_americaCW += 1
+@@antman += 1
+@@antmanATW += 1
+@@avengers += 1
+@@avengersAOU += 1
+@@avengersIW += 1
+@@gotg += 1
+@@gotg2 += 1
+@@doctor_strange += 1
+@@captain_marvel += 1
+@@black_panther += 1
+@@SMHomecoming  += 1
+  elsif string = "DC"
+@@TDK += 1
+@@TDKR += 1
+@@MOS += 1
+@@green_lantern += 1
+@@ww += 1
+@@ww1984 += 1
+@@shazam += 1
+@@BMvsSM += 1
+@@justice += 1
+@@suicide_squad += 1
+
+def oldornew(string)
+  if string == "old"
+@@iron_man += 1
+@@iron_man2 += 1
+@@thor += 1
+@@thorTDW += 1
+@@hulk += 1
+@@captain_americaFA += 1
+@@captain_americaTWS += 1
+@@antman += 1
+@@avengers += 1
+@@gotg += 1
+
+
+
+
+
+    
+    
+    
