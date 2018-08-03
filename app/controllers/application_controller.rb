351e9718f02a171d151ca18bcf435550107db36f
AderonkeAdejare <aderonkea@comcast.net>
2018-08-03 15:41:33 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,66 @@
-class ApplicationController < Sinatra::Base
+require 'bundler'
+Bundler.require
+require './app/models/biasquiz'
 
+class ApplicationController < Sinatra::Base
+  
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
   end
+end
 
-  get "/" do
-  	erb :index
+
+
+
+class SApp < Sinatra::Base
+  
+  configure do
+  	set :views, "app/views"
+  	set :public_dir, "public"
+  end
+  
+  
+  get "/" do 
+    erb:homepage
+  end
+  
+  get "/about" do 
+    erb :about
+  end
+  
+  get "/racism" do 
+    erb:racism
+  end
+
+  get "/biasquiz" do
+    erb:racismquiz
+  end
+  
+  post '/biasquiz' do 
+    answers = params.values
+    @total= 0
+    answers.each do |answers|
+      @total += answers.to_i
+    puts @total
+  end
+   
+   @combo = racismquiz(@total)
+    if @combo == "reallyracist"
+      erb :reallyracist
+    elsif @combo == "somewhatracist"
+      erb :somewhatracist
+    elsif @combo == "notracist"
+     erb :notracist
+   end   
+  end
+   
+  get "/impact" do 
+    erb:impact
+  end
+
+  get '/sexism' do 
+    erb:sexism
   end
 end
+  
