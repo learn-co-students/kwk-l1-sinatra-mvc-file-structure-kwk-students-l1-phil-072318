0e099aa410dd22781f7d4841e0f9df2d9c0147a9
LaurenWorkosky <19lworkosky@student.dasd.org>
2018-08-03 15:38:52 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,54 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-  	erb :index
+  	erb:index
+  end
+  
+  get '/food' do
+    erb:food
+  end
+  
+  get '/fitness' do
+    erb:fitness
+  end
+  
+  get '/mental' do
+    erb:mental
+  end
+  
+  get '/aboutus' do
+    erb:aboutus
+  end
+  
+  get '/foodwl' do
+    erb:foodwl
+  end
+  
+  get '/foodinspo' do
+    erb:foodinspo
+  end
+  
+  get '/foodwg' do
+    erb:foodwg
+  end
+  
+  get '/fitnessmg' do
+    erb:fitnessmg
+  end
+  
+  get '/fitnesst' do
+    erb:fitnesst
+  end
+  
+  get '/water' do
+    erb:water
+  end
+  
+  get '/fitnesswl' do
+    erb:fitnesswl
+  end
+  
+  get '/fitnessinspo' do
+    erb:fitnessinspo
   end
 end
